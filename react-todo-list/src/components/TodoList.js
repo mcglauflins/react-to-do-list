28724563dcaf923656d7e6cfee3f77907d790212
mcglauflins <sarahriebschlager@gmail.com>
2022-02-24 00:23:22 +0000

    todo work

---
+++ react-todo-list/src/components/TodoList.js
@@ -0,0 +1,26 @@
+import React, {useState} from 'react'
+import TodoForm from './TodoForm'
+
+function TodoList() {
+
+    const [todos, setTodos] = useState([]);
+
+    const addTodo = todo => {
+        if(!todo.text || /^\s*$/.test(todo.text)) {
+            return;
+        }
+
+        const newTodos = [todo, ...todos];
+
+        setTodos(newTodos);
+    }
+
+  return (
+    <div>
+    <h1>Whats on the List for Today?</h1>
+    <TodoForm onSubmit = {addTodo} />
+    </div>
+  );
+}
+
+export default TodoList
